8dbb1384d932070734f6be9602cd68b457677b39
MaybeAnonymous <89218161+MaybeAnonymous@users.noreply.github.com>
2022-12-12 14:04:19 -0300

    Changed Awesome layout order

--- HOME/.config/awesome/rc.lua
+++ HOME/.config/awesome/rc.lua
@@ -78,15 +78,15 @@ local mod = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile,
+    awful.layout.suit.spiral,
+    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.floating,
+    awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
