43c170993f1848336ff6d374f38ccc5af3be9553
MaybeAnonymous <89218161+MaybeAnonymous@users.noreply.github.com>
2023-01-20 15:17:45 -0300

    fixed minor error in hyprland autostart

--- HOME/.config/hypr/scripts/autostart.sh
+++ HOME/.config/hypr/scripts/autostart.sh
@@ -7,7 +7,7 @@ gammastep &
 
 # > GTK
 # --- Setup GTK Theme
-config="${$HOME}/.config/gtk-3.0/settings.ini"
+config="${HOME}/.config/gtk-3.0/settings.ini"
 if [ ! -f "$config" ]; then exit 1; fi
 
 gnome_schema="org.gnome.desktop.interface"
